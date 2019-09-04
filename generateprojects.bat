8b4d0a5ddb2ab6e0e747fcad04936b26e9450bc3
jonli <jonli@tencent.com>
2019-09-04 19:43:57 +0800

    include tools

--- generateprojects.bat
+++ generateprojects.bat
@@ -1,3 +1,3 @@
 cd bgfx
-..\bx\tools\bin\windows\genie --with-windows=10.0.17763.0 --with-examples vs2017
+..\bx\tools\bin\windows\genie --with-tools --with-windows=10.0.17763.0 --with-examples vs2017
 rem start .build\projects\vs2017\bgfx.sln
